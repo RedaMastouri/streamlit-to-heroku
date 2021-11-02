5ff1ad908741b41554417d9d4eda5e369560e164
Reda Mastouri <reda.mastouri@rutgers.edu>
2021-11-02 04:32:43 -0400

    Type in a message

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#Create non existing config folder 
+mkdir ~/.streamlit
+#Create config file
+touch ~/.streamlit/config.toml
+#Init the config
+streamlit config show > ~/.streamlit/config.toml
+#override the config 
+mkdir -p ~/.streamlit/echo "[server]\n headless = true\n port = $PORT\n enableCORS = false\n \n" > ~/.streamlit/config.toml
